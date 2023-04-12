2cadf3bd6854a67c12c421eb374f2cc6ad68c3a5
artsol0 <artur200360@gmail.com>
2023-04-12 13:47:13 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Button, Glyphicon } from 'react-bootstrap';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  handleClick() {
+    // console.log('done!')
+
+    axios.get('https://api.github.com/users/maecapozzi')
+    //.then(response => console.log(response));
+    .then(response => this.setState({id:response.data.id}))
+  }
+  constructor() {
+    super();
+    this.state = {
+      id: ''
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='button_container'>
+          <button className='button' onClick={this.handleClick}>push me</button>
+          <p>{this.state.id}</p>
+        </div>
+        <Button type="button" className="btn btn-info">
+          <span class="glyphicon glyphicon-search"></span> Search
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
